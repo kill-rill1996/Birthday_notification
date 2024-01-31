60c9a7d3887a84d25517673655e600ce2283368e
Alexandr Andreev <aleksandrandreev@MacBook-Pro-Aleksandr.local>
2024-01-31 19:19:18 +0300

    Add 'set_commands' func

--- services/bot_telegram.py
+++ services/bot_telegram.py
@@ -6,11 +6,13 @@ from .handlers import dp, dp_register_handlers
 
 
 from config import TOKEN
+from .utils import set_commands
 
 
 async def init_bot() -> None:
     # Initialize Bot instance with a default parse mode which will be passed to all API calls
     bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    await set_commands(bot)
     dp_register_handlers(dp)
 
     await dp.start_polling(bot)
--- services/utils.py
+++ services/utils.py
@@ -1,5 +1,8 @@
 from datetime import date, datetime
 
+from aiogram import Bot
+from aiogram.types import BotCommand, BotCommandScopeDefault
+
 from services.errors import DateValidationError
 
 
@@ -11,4 +14,17 @@ def parse_birthday_date(message: str):
     except Exception:
         raise DateValidationError
     return result
+
+
+async def set_commands(bot: Bot):
+    commands = [
+        BotCommand(command="start", description="Запуск бота"),
+        BotCommand(command="registration", description="Регистрация"),
+        BotCommand(command="events", description="Ближайшие события"),
+        BotCommand(command="update", description="Изменение профиля"),
+        BotCommand(command="delete", description="Удаление профиля"),
+        BotCommand(command="help", description="Справка"),
+    ]
+
+    await bot.set_my_commands(commands, BotCommandScopeDefault())
 
